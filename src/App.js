7c4100ffe3d7e2b296d666cf442e930f466d3c20
luca7084 <63428345+luca7084@users.noreply.github.com>
2020-10-29 05:37:10 -0300

    Arquivos da calculadora com memória

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
 
-
-
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {atual: "0", antigo: ""};
+        this.state = {atual: "0", antigo: "", memoria:[], topo:""};
     
 
     this.numero = this.numero.bind(this);
@@ -15,6 +13,12 @@ class App extends React.Component {
     this.virgula = this.virgula.bind(this);
     this.operador = this.operador.bind(this);
     this.igual = this.igual.bind(this);
+    this.limparmem = this.limparmem.bind(this);
+    this.ultmem = this.ultmem.bind(this);
+    this.somamem = this.somamem.bind(this);
+    this.salvamem = this.salvamem.bind(this);
+    this.recmem = this.recmem.bind(this);
+    this.apagamem = this.apagamem.bind(this);
 
     }
 
@@ -56,7 +60,6 @@ class App extends React.Component {
                 : state.atual + ",",
             };
         });
-
     }
 
     operador(sinal) {
@@ -95,73 +98,158 @@ class App extends React.Component {
         }
     }
 
+    limparmem() {
+        this.setState(state =>{
+            return{
+                memoria: [],
+                topo: "",
+            };
+        });
+    }
+
+    ultmem() {
+        this.setState(state =>{
+            return{
+                atual: state.topo,
+            };
+        });
+    }
+
+    somamem() {
+        if (this.state.memoria === []) {
+            return;
+        }
+        else {
+            let n1 = this.state.atual;
+            let n2 = this.state.topo;
+            let res = eval((n1.replaceAll(".", "")).replaceAll(",",".")+"+"+(n2.replaceAll(".", "")).replaceAll(",", ".")).toString();
+            let resf = formatar(res.replaceAll(".", ","));
+            let elemento = new Valmem(resf);
+            let memoria = [...this.state.memoria];
+            memoria.pop();
+            memoria.push(elemento);
+            this.setState(state =>{
+                return{
+                    topo: resf,
+                    memoria,
+                };
+            });
+        }
+    }
+
+    salvamem() {
+        let elemento = new Valmem(this.state.atual);
+        let memoria = [...this.state.memoria];
+        memoria.push(elemento);
+        this.setState(state =>{
+            return{
+                memoria,
+                topo: state.atual,
+            };
+        });
+    }
+
+    recmem(val) {
+        this.setState(state =>{
+            return{
+                atual: val,
+            };
+        });
+    }
+
+    apagamem(val) {
+        this.setState(state =>{
+            return{
+                memoria: state.memoria.filter(x => x.val !== val),
+            };
+        });
+    }
+
     render() {
-        const {atual, antigo} = this.state;
+        const {atual, antigo, memoria} = this.state;
         return (
             <div className="App">
                 <header>
                     <h1>Lucalculadora</h1>
                 </header>
                 <div className="corpo">
-                    <span className="tela">
-                        <div className="mostra-antigo">{antigo}</div>
-                        <div className="mostra-atual">{atual}</div>
-                    </span>
-                    <div className="butts">
-                        <Botnum id="zero" onClick={() => this.numero("0")}>
-                            0
-                        </Botnum>
-                        <Botnum id="un" onClick={() => this.numero("1")}>
-                            1
-                        </Botnum>
-                        <Botnum id="deux" onClick={() => this.numero("2")}>
-                            2
-                        </Botnum>
-                        <Botnum id="trois" onClick={() => this.numero("3")}>
-                            3
-                        </Botnum>
-                        <Botnum id="quatre" onClick={() => this.numero("4")}>
-                            4
-                        </Botnum>
-                        <Botnum id="cinq" onClick={() => this.numero("5")}>
-                            5
-                        </Botnum>
-                        <Botnum id="six" onClick={() => this.numero("6")}>
-                            6
-                        </Botnum>
-                        <Botnum id="sept" onClick={() => this.numero("7")}>
-                            7
-                        </Botnum>
-                        <Botnum id="huit" onClick={() => this.numero("8")}>
-                            8
-                        </Botnum>
-                        <Botnum id="neuf" onClick={() => this.numero("9")}>
-                            9
-                        </Botnum>
-                        <Botlim id="limpar" onClick={() => this.limpar()}>
-                            CE
-                        </Botlim>
-                        <Botapa id="apagar" onClick={() => this.apagar()}>
-                            DEL
-                        </Botapa>
-                        <Botvir id="virgula" onClick={() => this.virgula()}>
-                            ,
-                        </Botvir>
-                        <Botope id="soma" onClick={() => this.operador("+")}>
-                            +
-                        </Botope>
-                        <Botope id="subtracao" onClick={() => this.operador("-")}>
-                            -
-                        </Botope>
-                        <Botope id="multiplicacao" onClick={() => this.operador("*")}>
-                            *
-                        </Botope>
-                        <Botope id="divisao" onClick={() => this.operador("/")}>
-                            /
-                        </Botope>
-                        <Botigu id="igual" onClick={() => this.igual()}>
-                            =
-                        </Botigu>
+                    <div className="processos">
+                        <span className="tela">
+                            <div className="mostra-antigo">{antigo}</div>
+                            <div className="mostra-atual">{atual}</div>
+                        </span>
+                        <div className="butts">
+                            <Botnum id="zero" onClick={() => this.numero("0")}>
+                                0
+                            </Botnum>
+                            <Botnum id="un" onClick={() => this.numero("1")}>
+                                1
+                            </Botnum>
+                            <Botnum id="deux" onClick={() => this.numero("2")}>
+                                2
+                            </Botnum>
+                            <Botnum id="trois" onClick={() => this.numero("3")}>
+                                3
+                            </Botnum>
+                            <Botnum id="quatre" onClick={() => this.numero("4")}>
+                                4
+                            </Botnum>
+                            <Botnum id="cinq" onClick={() => this.numero("5")}>
+                                5
+                            </Botnum>
+                            <Botnum id="six" onClick={() => this.numero("6")}>
+                                6
+                            </Botnum>
+                            <Botnum id="sept" onClick={() => this.numero("7")}>
+                                7
+                            </Botnum>
+                            <Botnum id="huit" onClick={() => this.numero("8")}>
+                                8
+                            </Botnum>
+                            <Botnum id="neuf" onClick={() => this.numero("9")}>
+                                9
+                            </Botnum>
+                            <Botlim id="limpar" onClick={() => this.limpar()}>
+                                CE
+                            </Botlim>
+                            <Botapa id="apagar" onClick={() => this.apagar()}>
+                                DEL
+                            </Botapa>
+                            <Botvir id="virgula" onClick={() => this.virgula()}>
+                                ,
+                            </Botvir>
+                            <Botope id="soma" onClick={() => this.operador("+")}>
+                                +
+                            </Botope>
+                            <Botope id="subtracao" onClick={() => this.operador("-")}>
+                                -
+                            </Botope>
+                            <Botope id="multiplicacao" onClick={() => this.operador("*")}>
+                                *
+                            </Botope>
+                            <Botope id="divisao" onClick={() => this.operador("/")}>
+                                /
+                            </Botope>
+                            <Botigu id="igual" onClick={() => this.igual()}>
+                                =
+                            </Botigu>
+                            <BotlimM id="lim" onClick={() => this.limparmem()}>
+                                MC
+                            </BotlimM>
+                            <BotultM id="ult" onClick={() => this.ultmem()}>
+                                MR
+                            </BotultM>
+                            <BotsomM id="som" onClick={() => this.somamem()}>
+                                M+
+                            </BotsomM>
+                            <BotsalM id="sal" onClick={() => this.salvamem()}>
+                                MS
+                            </BotsalM>
+                        </div>
+                    </div>
+                    <div className="armazenamento">
+                        <h2>Armazenamento</h2>
+                        <MostraMemoria memoria={memoria} recmem={this.recmem} apagamem={this.apagamem}></MostraMemoria>
                     </div>
                 </div>
             </div>
@@ -173,7 +261,7 @@ class App extends React.Component {
 
 function Botao(props) {
     const {onClick, className, children, id} = props;
-    return (
+    return(
         <button onClick={onClick} className={className} id={id}>
             {children}
         </button>
@@ -182,7 +270,7 @@ function Botao(props) {
 
 function Botnum(props) {
     const {onClick, children, id} = props;
-    return (
+    return(
         <Botao onClick={onClick} className="b-num" id={id}>
             {children}
         </Botao>
@@ -191,7 +279,7 @@ function Botnum(props) {
 
 function Botlim(props) {
     const {onClick, children, id} = props;
-    return (
+    return(
         <Botao onClick={onClick} className="b-lim" id={id}>
             {children}
         </Botao>
@@ -200,7 +288,7 @@ function Botlim(props) {
 
 function Botapa(props) {
     const {onClick, children, id} = props;
-    return (
+    return(
         <Botao onClick={onClick} className="b-apa" id={id}>
             {children}
         </Botao>
@@ -209,7 +297,7 @@ function Botapa(props) {
 
 function Botvir(props) {
     const {onClick, children, id} = props;
-    return (
+    return(
         <Botao onClick={onClick} className="b-vir" id={id}>
             {children}
         </Botao>
@@ -218,7 +306,7 @@ function Botvir(props) {
 
 function Botope(props) {
     const {onClick, children, id} = props;
-    return (
+    return(
         <Botao onClick={onClick} className="b-ope" id={id}>
             {children}
         </Botao>
@@ -227,8 +315,62 @@ function Botope(props) {
 
 function Botigu(props) {
     const {onClick, children, id} = props;
-    return (
+    return(
         <Botao onClick={onClick} className="b-igu" id={id}>
+            {children}
+        </Botao>
+    );
+}
+
+function BotlimM(props) {
+    const {onClick, children, id} = props;
+    return(
+        <Botao onClick={onClick} className="b-limM" id={id}>
+            {children}
+        </Botao>
+    );
+}
+
+function BotultM(props) {
+    const {onClick, children, id} = props;
+    return(
+        <Botao onClick={onClick} className="b-ultM" id={id}>
+            {children}
+        </Botao>
+    );
+}
+
+function BotsomM(props) {
+    const {onClick, children, id} = props;
+    return(
+        <Botao onClick={onClick} className="b-somM" id={id}>
+            {children}
+        </Botao>
+    );
+}
+
+function BotsalM(props) {
+    const {onClick, children, id} = props;
+    return(
+        <Botao onClick={onClick} className="b-salM" id={id}>
+            {children}
+        </Botao>
+    );
+}
+
+function BotrecM(props) {
+    const {onClick, children, id} = props;
+    return(
+        <Botao onClick={onClick} className="b-recM" id={id}>
+            {children}
+        </Botao>
+    );
+}
+
+function BotapaM(props) {
+    const {onClick, children, id} = props;
+    return(
+        <Botao onClick={onClick} className="b-apaM" id={id}>
             {children}
         </Botao>
     );
@@ -238,6 +380,33 @@ function formatar(num) {
     let saida=parseFloat((num.replaceAll(".", "")).replaceAll(",", "."));
     saida=Intl.NumberFormat("pt-BR", {maximumFractionDigits:9}).format(saida);
     return saida;
+}
+
+function Valmem(val) {
+    this.val = val;
+}
+
+function MostraMemoria(props) {
+    const {memoria, recmem, apagamem} = props;
+    return(
+        <table className="tabela">
+            <tbody>
+                {memoria.map(x =>{
+                    return(
+                        <tr key={x.val}>
+                            <td className="tabela-display">{x.val}</td>
+                            <td>
+                                <BotrecM onClick={() => recmem(x.val)}><b>MR</b></BotrecM>
+                            </td>
+                            <td>
+                                <BotapaM onClick={() => apagamem(x.val)}><b>MS</b></BotapaM>
+                            </td>
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+    );
 }
 
 export default App;
